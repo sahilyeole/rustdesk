5c9e068398bef2442a37a4448fed110de7663792
Sahil Yeole <sahilyeole93@gmail.com>
2023-09-14 16:34:08 +0530

    revert optional check
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -330,11 +330,6 @@ class _GeneralState extends State<_General> {
       child: _OptionCheckBox(context, "Always use software rendering",
           'allow-always-software-render'),
     ));
-    children.add(
-            _OptionCheckBox(context, 'Check for software update on startup.','enable-check-update',
-            isServer: false,
-            ));
-            debugPrint(mainGetLocalBoolOptionSync('enable-check-update').toString());
     if (bind.mainShowOption(key: 'allow-linux-headless')) {
       children.add(_OptionCheckBox(
           context, 'Allow linux headless', 'allow-linux-headless'));
